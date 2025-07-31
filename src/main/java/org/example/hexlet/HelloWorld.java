aa71579356e1236f490ae64f3d74fe16b126a989
wasiliyterkin46 <wasiliy_terkin@mail.ru>
2025-07-31 18:06:15 +0300

    Add feature: processing parameters in a request

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -8,8 +8,13 @@ public class HelloWorld {
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
         });
+
         // Описываем, что загрузится по адресу /
-        app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/hello", ctx -> {
+            var name = ctx.queryParamAsClass("name", String.class).getOrDefault("World");
+            ctx.result(String.format("Hello, %s!", name));
+        });
+        
         app.start(7070); // Стартуем веб-сервер
     }
 }
