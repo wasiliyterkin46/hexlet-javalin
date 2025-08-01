680265d19541a26f973cacbf65d6789e3c28b83a
wasiliyterkin46 <wasiliy_terkin@mail.ru>
2025-08-01 10:01:53 +0300

    Add example dynamic route

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -9,13 +9,25 @@ public class HelloWorld {
             config.bundledPlugins.enableDevLogging();
         });
 
-        // Описываем, что загрузится по адресу /
+        addHelloName(app);
+        addDynamicRoute(app);
+
+        app.start(7070); // Стартуем веб-сервер
+    }
+
+    private static void addHelloName(Javalin app) {
         app.get("/hello", ctx -> {
             var name = ctx.queryParamAsClass("name", String.class).getOrDefault("World");
             ctx.result(String.format("Hello, %s!", name));
         });
-        
-        app.start(7070); // Стартуем веб-сервер
+    }
+
+    private static void addDynamicRoute(Javalin app) {
+        app.get("/users/{id}/lessons/{postId}", ctx -> {
+            var id = ctx.pathParam("id");
+            var postId =  ctx.pathParam("postId");
+            ctx.result("User ID: " + id + " Post ID: " + postId);
+        });
     }
 }
 
